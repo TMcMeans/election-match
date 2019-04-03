fd062e9e2009e754b7ea6ebeeb47530c230102a3
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-04-03 11:32:43 -0600

    Add header to apiCall to allow cross origin access to API

--- src/helpers/apicalls.js
+++ src/helpers/apicalls.js
@@ -2,6 +2,7 @@ export const fetchElections = async url => {
   let response = await fetch(url, {
     method: 'GET',
     headers: {
+      'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/json'
     }
   });
