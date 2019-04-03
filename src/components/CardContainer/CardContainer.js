2bbdd7d26556efeb951bdcfa9bbe0bb9e45cb26b
Tanjie McMeans <tanjmcmeans01@gmail.com>
2019-04-03 11:26:52 -0600

    Make CardContainer functional component

---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CardContainer = ({ electionData, error }) => {
+  return (
+    <div className="card-container">
+      <h1>Election Data</h1>
+    </div>
+  );
+};
